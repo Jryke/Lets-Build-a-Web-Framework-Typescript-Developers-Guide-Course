a5882f01c2f7dd6d3b5129648e21f6271c584793
Jesse Rykerson <rykerson@gmail.com>
2022-09-02 01:32:10 -0400

    Retreiving User Properties

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -117,11 +117,54 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"src/index.ts":[function(require,module,exports) {
+})({"src/models/User.ts":[function(require,module,exports) {
 "use strict";
 
-console.log("Hi there!");
-},{}],"../../../../../../.nvm/versions/node/v16.13.1/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.User = void 0;
+
+var User = /*#__PURE__*/function () {
+  function User(data) {
+    _classCallCheck(this, User);
+
+    this.data = data;
+  }
+
+  _createClass(User, [{
+    key: "get",
+    value: function get(propName) {
+      return this.data[propName];
+    }
+  }]);
+
+  return User;
+}();
+
+exports.User = User;
+},{}],"src/index.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var User_1 = require("./models/User");
+
+var user = new User_1.User({
+  name: "myname",
+  age: 20
+});
+console.log(user.get("name"));
+console.log(user.get("age"));
+},{"./models/User":"src/models/User.ts"}],"../../../../../../.nvm/versions/node/v16.13.1/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
