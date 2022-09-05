eaa78221e0b64eeddcdcec7f675dd9dd1dac67b3
Jesse Rykerson <rykerson@gmail.com>
2022-09-05 11:44:28 -0400

    TS problems fixed

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { UserForm } from "./views/UserForm";
 
-const userForm = new UserForm(document.getElementById("root"));
+const userForm = new UserForm(document.getElementById("root")!);
 
 userForm.render();
